d82b7d177838b45c7c2a23da1a58c75bf3711731
vcntdev <198897459+vcntdev@users.noreply.github.com>
2026-03-31 14:39:08 +0200

    feat: init main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import load_dotenv
+from random import randint
+import utils.seed as seed
+from app.object import user, organization
+
+load_dotenv.load_dotenv()
+
+# initialize final lunogram object
+class lunogram:
+    def __init__(self, api_key):
+        self.user = user(api_key)
+        self.organization = organization(api_key)
+
+# Seeder data to generate a random user should you need it, this is mainly for testing purposes
+def random_user():
+    firstname = seed.user.firstname()
+    lastname = seed.user.lastname()
+    email = seed.user.email(firstname, lastname)
+    phone = seed.user.phone()
+
+    external_id = f"user_{randint(10000, 99999)}"
+    anonymous_id = f"anon_{randint(1000, 9999)}"
+
+    return {
+        "identifier": [{
+            "source": "default",
+            "external_id": external_id,
+            "metadata": None,
+            "created_at": "2025-11-19T14:18:42.960Z",
+            "updated_at": "2025-11-23T17:20:00.021Z"
+        }],
+        "external_id": external_id,
+        "anonymous_id": anonymous_id,
+        "email": email,
+        "phone": phone,
+        "timezone": "Europe/Amsterdam",
+        "locale": "nl-NL",
+        "data": {
+            "first_name": firstname,
+            "last_name": lastname,
+            "has_completed_onboarding": True
+        }
+    }
+def main() -> None:
+    """
+    Example use ->
+    
+    client = lunogram("Your API key here")
+
+    > All sdk actions return a list with the api response on index 0, possible errors on index 1
+
+    req = client.user.upsert(random_user())
+    print(req[0])
+    """
+
+if __name__ == "__main__":
+    main()
