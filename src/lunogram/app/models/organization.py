225b8693a19c75653ccafd92b860ded11a68191f
vcntdev <198897459+vcntdev@users.noreply.github.com>
2026-04-21 14:22:50 +0200

    feat: partial implementation of organization typesafety

---
+++ src/lunogram/app/models/organization.py
@@ -0,0 +1,21 @@
+from src.lunogram.app.models.objects import events, scheduled
+from src.lunogram.app.http import httphandler
+from src.lunogram.app.types.organization import *
+
+from ...utils.reference import client
+
+class organization:
+    def __init__(self, api_key):
+        self.events = events(api_key, entity='organization')
+        self.scheduled = scheduled(api_key, entity='organization')
+        self.handler = httphandler(api_key)
+
+    def upsert(self, data: OrganizationEvent):
+        req = self.handler.post(client.organization.base, data)
+
+        return req
+    
+    def delete(self, data: OrganizationEvent):
+        req = self.handler.delete(client.organization.base, data)
+
+        return req
